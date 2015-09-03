2e067945ab8b520fd7a4e56499d39a6bd867ce71
Darryl Blake <darryl@darrylblake.com>
2015-09-03 07:13:07 -0700

    Moved local settings into untracked config file.

--- db/db.js
+++ db/db.js
@@ -1,11 +1,12 @@
 // Bring Mongoose into the app 
 var mongoose = require( 'mongoose' );
+var config = require( '../config' );
 
 // Get the connection string 
-var dbConnection = process.env.DB || ''
+var dbURI = process.env.DB_URI || config.dbURI
 
 // Create the database connection 
-var db = mongoose.connect(dbConnection);
+var db = mongoose.connect(dbURI);
 
 mongoose.connection.on('connected', function () {
   console.log('Mongoose Connected!');
--- routes/search.js
+++ routes/search.js
@@ -2,24 +2,14 @@ var express    = require('express');
 var https      = require('https');
 var router     = express.Router();
 var app        = express();
-
-var request = require('request');
-
-var client_id = process.env.FSQR_ID || ''
-var client_secret = process.env.FSQR_SECRET || ''
+var config     = require('../config');
+var request    = require('request');
 
 // require databases in case we need to access this information to compare to radius search
-var Venue      = require('./../db/Venue');
-// var User       = require('./../db/User');
-// var Comment    = require('./../db/Comment');
+var Venue = require('./../db/Venue');
 
-var options = {
-  url: 'https://api.github.com/repos/request/request',
-  headers: {
-    'User-Agent': 'request',
-    'content-type': 'text/json'
-  }
-};
+var fsID = process.env.FSQR_ID || config.fs.id;
+var fsSecret = process.env.FSQR_SECRET || config.fs.secret;
 
 // helper function to map relevant venue data to array
 var mapData = function(body) {
@@ -36,29 +26,25 @@ var mapData = function(body) {
 
 // api search request to search by keyword
 router.get('/query/:query/:latlong', function(req, res){
-  // 
   request('https://api.foursquare.com/v2/venues/search?ll=' + req.params.latlong + 
           '&query=' + req.params.query + 
-          '&client_id=' + client_id + 
-          '&client_secret=' + client_secret + 
+          '&client_id=' + fsID + 
+          '&client_secret=' + fsSecret + 
           '&v=20150831' +
           '&limit=10', function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      console.log(JSON.parse(body).length);
-
       res.send(mapData(body));
     }
   });
 });
 
 // sample search request for HR: http://localhost:8000/api/search/radius/1/37.783542,-122.408943/
-
 // api search request by radius based on long/lat input
 router.get('/radius/:radius/:latlong', function(req, res){
   request('https://api.foursquare.com/v2/venues/search?ll=' + req.params.latlong + 
           '&radius=' + req.params.radius + 
-          '&client_id=' + client_id + 
-          '&client_secret=' + client_secret + 
+          '&client_id=' + fsID + 
+          '&client_secret=' + fsSecret + 
           '&v=20150831' +
           '&limit=10', function (error, response, body) {
     if (!error && response.statusCode == 200) {
